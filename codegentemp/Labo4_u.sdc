e9e6da9d7e5339173655bd504e566c166d9a0b3c
Markedydoodle <59941873+Markedydoodle@users.noreply.github.com>
2021-03-20 23:41:55 -0400

    Part 2 + tweak Part 1
    Did Part 2 in its entirety
    Changed vTaskDelay ticks counter to ms counter

--- codegentemp/Labo4_u.sdc
+++ codegentemp/Labo4_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\Users\markm\Documents\PSoC Creator\Labo4\Labo4.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\Users\markm\Documents\PSoC Creator\Labo4\Labo4.cydsn\Labo4.cyprj
-# Date: Sun, 21 Mar 2021 00:57:44 GMT
+# Date: Sun, 21 Mar 2021 02:11:06 GMT
--- main_cm4.c
+++ main_cm4.c
@@ -16,29 +16,78 @@
 #include "params.h"
 #include "queue.h"
 
+//Part 2 variable globale
+SemaphoreHandle_t bouton_semph=NULL; //...
+int button_count = 0;
+
+//Part 1
 void LED_TASK()
 {
     for(;;)
     {
         Cy_GPIO_Write(RED_PORT,RED_NUM,1);
-        vTaskDelay(500);
+        vTaskDelay(pdMS_TO_TICKS(500));
         Cy_GPIO_Write(RED_PORT,RED_NUM,0);
-        vTaskDelay(500);
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 
 }
 
 
+//Part 2
+void isr_bouton()
+{
+    CyDelay(50);
+    if(button_count==2)
+    {button_count=0;}
+    
+    button_count++;
+    xSemaphoreGiveFromISR(bouton_semph,NULL);
+    Cy_GPIO_ClearInterrupt(Bouton_0_PORT,Bouton_0_NUM);
+    NVIC_ClearPendingIRQ(bouton_isr_cfg.intrSrc);
+    
+}
+
+void bouton_task()
+{     
+    for(;;)
+    {
+        if(bouton_semph!=NULL)          //s'il a ete creer et peut etre utilise
+        {
+            if(xSemaphoreTake(bouton_semph,pdMS_TO_TICKS(20))== pdTRUE)       //la 2e variable représente le temps en ticks pr attendre que sémaphore devient 
+            {
+                if(button_count==1)
+                {UART_PutString(" Bouton appuye ");
+                UART_PutString("\r\n");}
+                else if (button_count==2)
+                {UART_PutString(" Bouton relache ");
+                UART_PutString("\r\n");}
+            }
+        }
+    }
+    xSemaphoreGive(bouton_semph);
+}
+
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
 
+    //Part 2
+    bouton_semph = xSemaphoreCreateBinary();
     
-    xTaskCreate(LED_TASK,"LED_TASK",80,NULL,1,0);
+    Cy_SysInt_Init(&bouton_isr_cfg, isr_bouton);
+    NVIC_ClearPendingIRQ(bouton_isr_cfg.intrSrc);
+    NVIC_EnableIRQ(bouton_isr_cfg.intrSrc);
+    
+    UART_Start();
+    
+    //Part 1
+    xTaskCreate(LED_TASK,"LED_TASK",80,NULL,3,NULL);
+    xTaskCreate(bouton_task,"samaphore_TASK",400,NULL,1,NULL);
     vTaskStartScheduler();
     for(;;)
     {
-        /* Place your application code here. */
+        
     }
 }
 
