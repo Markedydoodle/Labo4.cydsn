97155a989047d13251f1cce05aa1d6077b2a376c
Markedydoodle <59941873+Markedydoodle@users.noreply.github.com>
2021-03-20 21:07:45 -0400

    Part 1
    Création d'un LED_TASK qui alterne entre allumer et fermer un LED

--- Generated_Source/PSoC6/cyfitter_cfg.c
+++ Generated_Source/PSoC6/cyfitter_cfg.c
@@ -277,6 +277,23 @@ void Cy_SystemInit(void)
 	/* Clock */
 	ClockInit();
 
+	/* Port0 configuration */
+	{
+	    const cy_stc_gpio_prt_config_t port0_cfg =
+	    {
+	        .out        = 0x00000000u,
+	        .intrMask   = 0x00000000u,
+	        .intrCfg    = 0x00000000u,
+	        .cfg        = 0x00006000u,
+	        .cfgIn      = 0x00000000u,
+	        .cfgOut     = 0x00000000u,
+	        .cfgSIO     = 0x00000000u,
+	        .sel0Active = 0x00000000u,
+	        .sel1Active = 0x00000000u,
+	    };
+	    (void)Cy_GPIO_Port_Init(GPIO_PRT0, &port0_cfg);
+	}
+
 	/* Port6 configuration */
 	{
 	    const cy_stc_gpio_prt_config_t port6_cfg =
--- main_cm4.c
+++ main_cm4.c
@@ -16,12 +16,26 @@
 #include "params.h"
 #include "queue.h"
 
+void LED_TASK()
+{
+    for(;;)
+    {
+        Cy_GPIO_Write(RED_PORT,RED_NUM,1);
+        vTaskDelay(500);
+        Cy_GPIO_Write(RED_PORT,RED_NUM,0);
+        vTaskDelay(500);
+    }
+
+}
+
+
 int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
 
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
+    
+    xTaskCreate(LED_TASK,"LED_TASK",400,NULL,1,0);
+    vTaskStartScheduler();
     for(;;)
     {
         /* Place your application code here. */
